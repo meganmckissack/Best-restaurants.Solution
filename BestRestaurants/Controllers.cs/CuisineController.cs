b8168085e01d5f43e1dee4dba5119f904d69340a
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-25 19:23:36 -0700

    add Delete functionality

--- BestRestaurants/Controllers.cs/CuisineController.cs
+++ BestRestaurants/Controllers.cs/CuisineController.cs
@@ -54,5 +54,20 @@ namespace BestRestaurants.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult Delete(int id)
+        {
+            var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+            return View(thisCuisine);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+            _db.Cuisines.Remove(thisCuisine);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
